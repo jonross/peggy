41090d81c290e5834f7e13138e0b23fb07dc61fb
Jonathan Ross <jonross@alum.mit.edu>
2013-05-12 21:25:25 -0400

    Fix bugs, add Flatten(), write sample calculator.

--- peg.go
+++ peg.go
@@ -4,7 +4,7 @@ Package peg is a PEG-based parser.
 package peg
 
 import (
-    "fmt"
+    "log"
     "reflect"
     "strings"
     "unicode"
@@ -12,7 +12,9 @@ import (
 
 type Parser struct {
     // for debugging
-    info string
+    description string
+    // non-nil if this is only a proxy for another parser
+    delegate *Parser
     // if false, allows fast rejection of empty strings by invocation wrapper
     allowEmpty bool
     // if true all subsidiary parsers don't skip whitespace
@@ -25,6 +27,12 @@ type Parser struct {
     handler func(info *Info) interface{}
     // what to pass to the handler
     context Info
+    // do we try to flatten user result arrays
+    flatten bool
+    // and how far
+    howFlat int
+    // debug depth when Parse is called
+    debug int
 }
 
 // This is passed to user callbacks.  Parser field is private because we don't want the user
@@ -43,6 +51,8 @@ type state struct {
     noSkip int
     // recursion depth
     depth int
+    // debug depth
+    debug int
 }
 
 // Return a Parser that matches any character in a string.
@@ -59,7 +69,15 @@ func AnyOf(str string) *Parser {
     })
 }
 
-// Return a Parser that matches a literal string in the input.
+// Return a Parser that will match what another parser later specified with Bind() matches.
+// TODO: helpful error message if user neglects to call Bind()
+//
+func Deferred() *Parser {
+    return newParser("Proxy", false, nil, nil)
+}
+
+// Return a Parser that matches a literal string in the input; also establishes
+// a default Handler that returns the text value.
 //
 func Literal(str string) *Parser {
     runes := []rune(str)
@@ -75,7 +93,7 @@ func Literal(str string) *Parser {
             }
         }
         return true, strLen, nil
-    })
+    }).Handle(func(info *Info) interface{} { return info.Text() })
 }
 
 // Return a Parser that tries a list of parsers in succession
@@ -169,15 +187,40 @@ func Sequence(parsers ...*Parser) *Parser {
 //
 func newParser(info string, allowEmpty bool, subParsers []*Parser, 
                parse func(state *state, input []rune) (bool, int, interface{})) *Parser {
-    return &Parser{info, allowEmpty, false, parse, subParsers, nil, Info{}}
+    return &Parser{info, nil, allowEmpty, false, parse, subParsers, nil, Info{}, false, 0, 0}
 }
 
 // Run one pass of a parser.  Skips whitespace if directed, and invokes
 // the handler with the string matched.
 //
 func (parser *Parser) invoke(state *state, input []rune) (bool, int, interface{}) {
-    fmt.Printf("%s-> %s on '%s'\n", strings.Repeat(" ", state.depth * 2), 
-                parser.info, string(input))
+
+    indent := func() string { return strings.Repeat(" ", state.depth * 4) }
+
+    if state.debug > 0 {
+        log.Printf("%s-> %s on '%s'\n", indent(), parser.description, string(input))
+    }
+
+    state.depth += 1
+    state.debug -= 1
+
+    var match bool
+    var used int
+    var result interface{}
+
+    defer func() {
+        state.depth -= 1
+        state.debug += 1
+        if state.debug > 0 {
+            log.Printf("%s<- %s %v, len=%d, result=%v", indent(), parser.description, match, used, result)
+        }
+    }()
+
+    if (parser.delegate != nil) {
+        match, used, result = parser.delegate.invoke(state, input)
+        return match, used, result
+    }
+
     // TODO: optimize to skip space once per each change of depth; right now
     // for e.g. OneOf we do it for each subsidiary match attempt
     space := parser.skipWhite(state, input)
@@ -187,23 +230,46 @@ func (parser *Parser) invoke(state *state, input []rune) (bool, int, interface{}
     if len(input) == 0 && !parser.allowEmpty {
         return false, 0, nil
     }
-    state.depth += 1
     if parser.adjacent {
         state.noSkip += 1
     }
-    match, used, result := parser.parse(state, input)
-    if match && parser.handler != nil {
-        parser.context.matched = input[:used]
-        parser.context.result = result
-        result = parser.handler(&parser.context)
+
+    match, used, result = parser.parse(state, input)
+
+    if match {
+        if parser.flatten {
+            if reflect.ValueOf(result).Kind() == reflect.Slice {
+                if state.debug > 0 {
+                    log.Printf("%sflatten -> %#v\n", indent(), result)
+                }
+                result = flatten(make([]interface{}, 0), result, parser.howFlat + 1)
+                if state.debug > 0 {
+                    log.Printf("%sflatten <- %#v\n", indent(), result)
+                }
+            } else {
+                if state.debug > 0 {
+                    log.Printf("%scan't flatten %#v\n", indent(), result)
+                }
+            }
+        }
+        if match && parser.handler != nil {
+            parser.context.matched = input[:used]
+            parser.context.result = result
+            if state.debug > 0 {
+                log.Printf("%sHandler => %#v\n", indent(), result)
+            }
+            result = parser.handler(&parser.context)
+            if state.debug > 0 {
+                log.Printf("%sHandler <= %#v\n", indent(), result)
+            }
+        }
     }
-    state.depth -= 1
+
     if parser.adjacent {
         state.noSkip -= 1
     }
-    fmt.Printf("%s%v, %d <- %s on '%s'\n", strings.Repeat(" ", state.depth * 2),
-                match, used, parser.info, string(input))
-    return match, used, result
+
+    return match, used + space, result
 }
 
 func (parser *Parser) skipWhite(state *state, input[] rune) int {
@@ -219,12 +285,43 @@ func (parser *Parser) skipWhite(state *state, input[] rune) int {
     return space
 }
 
+func flatten(a []interface{}, x interface{}, depth int) []interface{} {
+    if depth == 0 {
+        a = append(a, x)
+        return a
+    }
+    val := reflect.ValueOf(x)
+    if val.Kind() != reflect.Slice {
+        a = append(a, x)
+        return a
+    }
+    for i := 0; i < val.Len(); i++ {
+        a = flatten(a, val.Index(i).Elem().Interface(), depth - 1)
+    }
+    return a
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Set the adjacency flag to true, meaning all subsidiary parsers
 // must match with no intervening whitespace.
 func (parser *Parser) Adjacent() *Parser {
     parser.adjacent = true
+    return parser
+}
+
+// Set the debug level; n levels deep of parsers will log details of their execution.  Note this
+// applies only to the parser on which Parse() is called.
+//
+func (parser *Parser) Debug(depth int) *Parser {
+    parser.debug = depth
+    return parser
+}
+
+// Used with a Parser constructed with Deferred() -- specify the parser that will actually run.
+//
+func (parser *Parser) Bind(delegate *Parser) *Parser {
+    parser.delegate = delegate
     return parser
 }
 
@@ -238,15 +335,33 @@ func (parser *Parser) Handle(handler func(info *Info) interface{}) *Parser {
 
 // Change the information string of the parser, used during debugging
 //
-func (parser *Parser) Info(info string) *Parser {
-    parser.info = info
+func (parser *Parser) Describe(text string) *Parser {
+    parser.description = text
+    return parser
+}
+
+// Indicate the a parser should flatten N levels of its user result before passing to its
+// handler.  This is helpful for dealing with constructs that introduce unneeded nesting.
+// Sample Parser setups and possible results with and without flattening:
+//
+//     Sequence(a, OneOrMoreOf(b))
+//     Sequence(a, OneOrMoreOf(b)).Flatten(1)
+//
+//     [a, [b, b, b]]
+//     [a, b, b, b]
+//
+//  If depth is 0, flatten completely.
+//
+func (parser *Parser) Flatten(depth int) *Parser {
+    parser.flatten = true
+    parser.howFlat = depth
     return parser
 }
 
 // Parse a string and return results.
 //
 func (parser *Parser) Parse(input string) (bool, int, interface{}) {
-    return parser.invoke(&state{0, 0}, []rune(input))
+    return parser.invoke(&state{0, 0, parser.debug}, []rune(input))
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -255,6 +370,17 @@ func (parser *Parser) Parse(input string) (bool, int, interface{}) {
 //
 func (info *Info) Text() string {
     return string(info.matched)
+}
+
+// Returns the length of the user data array, if an array; else
+// returns 0.
+//
+func (info *Info) Len() int {
+    val := reflect.ValueOf(info.result)
+    if val.Kind() == reflect.Slice {
+        return val.Len()
+    }
+    return 0
 }
 
 // Return user data associated with the current Parser.  If index==0,
@@ -267,6 +393,6 @@ func (info *Info) Get(index int) reflect.Value {
         return val
     }
     // TODO proper error handling
-    return val.Index(index - 1)
+    return val.Index(index - 1).Elem()
 }
 
--- peg_test.go
+++ peg_test.go
@@ -2,6 +2,9 @@ package peg
 
 import (
     . "launchpad.net/gocheck"
+    "log"
+    "os"
+    "strconv"
     "testing"
 )
 
@@ -16,6 +19,8 @@ type TypeVar struct {
     varName *string
 }
 
+// This fixture still in progress, has some test code for helmet.
+//
 func (s *MySuite) TestBasics(c *C) {
 
     letter := AnyOf("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_")
@@ -32,11 +37,7 @@ func (s *MySuite) TestBasics(c *C) {
     typeVar := Sequence(identifier, identifier).
         Handle(func(info *Info) interface{} {
             typeSpec := info.Get(1).String()
-            var varName string
-            if info.Get(2).IsNil() {
-                return &TypeVar{nil, &typeSpec, nil}
-            }
-            varName = info.Get(2).String()
+            varName := info.Get(2).String()
             return &TypeVar{nil, &typeSpec, &varName}
         })
 
@@ -57,3 +58,90 @@ func (s *MySuite) TestBasics(c *C) {
     search := Sequence(typeVar, ZeroOrMoreOf(step))
     search.Parse("a b -> c d ->> e f")
 }
+
+// Simple calculator.  User data values are simply floats.
+//
+func (s *MySuite) TestCalculator(c *C) {
+
+    console, err := os.OpenFile("./test.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    log.SetOutput(console)
+
+    digits := OneOrMoreOf(AnyOf("0123456789")).Adjacent().Describe("digits")
+    point := Literal(".")
+
+    add := OneOf(Literal("+"), Literal("-"))
+    mul := OneOf(Literal("*"), Literal("/"))
+    lpar, rpar := Literal("("), Literal(")")
+
+    // matches 3, 3.5, .5; return float value
+    number := OneOf(digits, Sequence(Optional(digits), point, digits)).
+        Adjacent().Describe("number").
+        Handle(func(info *Info) interface{} {
+            val, _ := strconv.ParseFloat(info.Text(), 64)
+            return val
+        })
+
+    // Define parsers for the following BNF:
+    //
+    // expr1 := expr2 ( ('+' | '-') expr2 ) *
+    // expr2 := expr3 ( ('*' | '/') expr3 ) *
+    // expr3 := number | '(' expr1 ')'
+    //
+    // We define them in reverse order and use a Deferred() parser for
+    // expr1 to handle the recursive definition.
+
+    makeOp := func(info *Info) interface{} {
+        op := info.Get(1).String()
+        rhs := info.Get(2).Float()
+        return func(lhs float64) float64 { 
+            switch op {
+                case "+": return lhs + rhs
+                case "-": return lhs - rhs
+                case "*": return lhs * rhs
+                case "/": return lhs / rhs
+                default: panic("bad op: " + op)
+            }
+        }
+    }
+
+    evalOps := func(info *Info) interface{} {
+        val := info.Get(1).Float()
+        for i := 1; i < info.Len(); i++ {
+            fn := info.Get(i + 1).Interface().(func(float64) float64)
+            val = fn(val)
+        }
+        return val
+    }
+
+    stripParens := func(info *Info) interface{} {
+        return info.Get(2).Float()
+    }
+
+    expr1 := Deferred()
+    expr3 := OneOf(number, Sequence(lpar, expr1, rpar).Handle(stripParens)).Describe("expr3")
+
+    mulOps := ZeroOrMoreOf(Sequence(mul, expr3).Describe("mulop").Handle(makeOp)).Describe("mulops")
+    expr2 := Sequence(expr3, mulOps).Flatten(1).Describe("expr2").Handle(evalOps)
+
+    addOps := ZeroOrMoreOf(Sequence(add, expr2).Describe("addop").Handle(makeOp)).Describe("addops")
+    _xpr1 := Sequence(expr2, addOps).Flatten(1).Describe("expr1").Handle(evalOps)
+
+    expr1.Bind(_xpr1).Debug(4)
+
+    try := func(expr string, expected float64) {
+        _, _, result := expr1.Parse(expr)
+        actual := result.(float64)
+        c.Check(actual, Equals, expected)
+    }
+
+    try(" 1 + 2  + 3", 6.0)
+    try(" 1 + 2  * 3", 7.0)
+    try("(1 + 2) * 3", 9.0)
+    try("(1 + 2) / 3", 1.0)
+
+    try ("5 + (5 * 5) - ((5 + 5) / 5)", 28.0)
+}
+
